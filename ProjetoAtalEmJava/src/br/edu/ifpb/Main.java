da8674f0b074efd50b716e29fe7d48534e4b0142
rafaelfigueredog <rafaelfigueredog@gmail.com>
2020-12-08 00:08:15 -0300

    teste2 and renamed files

--- ProjetoAtalEmJava/src/br/edu/ifpb/Main.java
+++ ProjetoAtalEmJava/src/br/edu/ifpb/Main.java
@@ -1,12 +1,10 @@
 package br.edu.ifpb;
-import java.io.IOException;
-import java.util.Vector;
 import java.util.Scanner;
 import br.edu.ifpb.domain.*;
 
 public class Main {
 
-    private static Scanner input = new Scanner(System.in);
+    private static Scanner input = new Scanner(System.in);  
     
 
 }
--- ProjetoAtalEmJava/src/br/edu/ifpb/domain/Disciplina.java
+++ ProjetoAtalEmJava/src/br/edu/ifpb/domain/Disciplina.java
@@ -64,4 +64,6 @@ public class Disciplina {
     public Double getCargaDeTrabalho() {
         return cargaDeTrabalho;
     }
+
+    
 }
